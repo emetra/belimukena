f01c8ab0801cb9ceddbd6c4b54e130a85fd49f27
kamuishirou33 <handywijaya33@gmail.com>
2018-07-06 12:09:29 +0700

    ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,36 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component , ViewChild } from '@angular/core';
+import { Nav , Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { HomePage} from "../pages/home/home";
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild("content") nav:Nav;
   rootPage:any = TabsPage;
+  pages : Array<{
+    title : string,
+    component : any
+  }>;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen ) {
+    this.pages = [
+      { title: 'Home', component: TabsPage}
+    ];
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  open(pages)
+  {
+    this.nav.setRoot(pages.component);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {NavbarPage} from "../pages/navbar/navbar";
+import {CartPage} from "../pages/cart/cart";
+import {OtherPage} from "../pages/other/other";
 
 @NgModule({
   declarations: [
@@ -17,7 +20,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    NavbarPage,
+    CartPage,
+    OtherPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +35,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    NavbarPage,
+    CartPage,
+    OtherPage
   ],
   providers: [
     StatusBar,
