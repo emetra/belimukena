e210fcc5bb668c5600b1700cbf7e597b916da199
kamuishirou33 <handywijaya33@gmail.com>
2018-07-19 12:15:01 +0700

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
-import { HomePage} from "../pages/home/home";
 
 @Component({
   templateUrl: 'app.html'
@@ -24,7 +23,8 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      statusBar.show();
+      statusBar.backgroundColorByHexString("#FFC0CB");
       splashScreen.hide();
     });
   }
